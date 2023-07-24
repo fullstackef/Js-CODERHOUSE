2bb7e55eae5bc45f79a7929af961dcdf1bb830f9
emmanuel fioramonti <fullstackef@gmail.com>
2023-07-23 22:43:00 -0300

    entrega final JS coderhouse

--- js/app.js
+++ js/app.js
@@ -1,87 +1,121 @@
-document.addEventListener("DOMContentLoaded", function() {
+// JavaScript
+document.addEventListener("DOMContentLoaded", function () {
   const botonesAgregar = document.querySelectorAll(".btn-primary");
   const tablaBody = document.getElementById("Cuerpocarrito");
   const btnVaciarCarrito = document.getElementById("btnVaciarCarrito");
+  const totalCarrito = document.getElementById("totalCarrito"); // Agrega el elemento HTML para mostrar el total
 
-  botonesAgregar.forEach(function(boton) {
-    boton.addEventListener("click", function() {
+  let carrito = obtenerPedidosDeLocalStorage();
+
+  function agregarAlCarrito(producto, unidades) {
+    const productoExistente = carrito.find((item) => item.nombre === producto.nombre);
+
+    if (productoExistente) {
+      productoExistente.unidades += unidades;
+      productoExistente.subtotal += producto.precio * unidades;
+    } else {
+      carrito.push({
+        nombre: producto.nombre,
+        unidades: unidades,
+        subtotal: producto.precio * unidades,
+      });
+    }
+
+    guardarPedidoEnLocalStorage();
+    mostrarCarrito();
+  }
+
+  function agregarEventListenersBotonesRemover() {
+    const botonesRemover = document.querySelectorAll(".btn-remover");
+    botonesRemover.forEach((boton) => {
+      boton.addEventListener("click", () => {
+        const nombreProducto = boton.getAttribute("data-nombre");
+        removerDelCarrito(nombreProducto);
+      });
+    });
+  }
+
+  function removerDelCarrito(nombreProducto) {
+    const productoExistente = carrito.find((item) => item.nombre === nombreProducto);
+
+    if (productoExistente) {
+      if (productoExistente.unidades > 1) {
+        productoExistente.unidades -= 1;
+        productoExistente.subtotal -= productoExistente.precio;
+      } else {
+        carrito = carrito.filter((item) => item.nombre !== nombreProducto);
+      }
+    }
+
+    guardarPedidoEnLocalStorage();
+    mostrarCarrito();
+  }
+
+  function mostrarCarrito() {
+    tablaBody.innerHTML = "";
+
+    carrito.forEach((item) => {
+      const fila = document.createElement("tr");
+      fila.innerHTML = `
+        <td>${item.nombre}</td>
+        <td>${item.unidades}</td>
+        <td>$${item.subtotal.toFixed(2)}</td>
+        <td><button class="btn btn-danger btn-remover" data-nombre="${item.nombre}">Remover</button></td>
+      `;
+      tablaBody.appendChild(fila);
+    });
+
+    agregarEventListenersBotonesRemover();
+    actualizarTotal();
+  }
+
+  function actualizarTotal() {
+    const total = carrito.reduce((acc, item) => acc + item.subtotal, 0);
+    totalCarrito.textContent = `$${total.toFixed(2)}`;
+  }
+
+  botonesAgregar.forEach(function (boton) {
+    boton.addEventListener("click", function () {
       const card = boton.closest(".card");
       const nombreProducto = card.querySelector(".card-title").textContent;
-      const precioProducto = parseFloat(card.querySelector(".card-text").textContent.slice(1));
-      const unidades = parseInt(card.querySelector(".form-select").value);
-      const subtotal = precioProducto * unidades;
+      const producto = productos.find((p) => p.nombre === nombreProducto);
 
-      // Verificar si el producto ya existe en la lista del carrito
-      const filasExistentes = tablaBody.querySelectorAll("tr");
-      let productoExistente = null;
-
-      filasExistentes.forEach(function(fila) {
-        const nombreFila = fila.querySelector("td:first-child").textContent;
-
-        if (nombreFila === nombreProducto) {
-          productoExistente = fila;
-        }
-      });
-
-      if (productoExistente) {
-        // Actualizar la cantidad y subtotal del producto existente
-        const cantidadExistente = parseInt(productoExistente.querySelector("td:nth-child(2)").textContent);
-        const subtotalExistente = parseFloat(productoExistente.querySelector("td:nth-child(3)").textContent.slice(1));
-
-        const nuevaCantidad = cantidadExistente + unidades;
-        const nuevoSubtotal = subtotalExistente + subtotal;
-
-        productoExistente.querySelector("td:nth-child(2)").textContent = nuevaCantidad;
-        productoExistente.querySelector("td:nth-child(3)").textContent = `$${nuevoSubtotal.toFixed(2)}`;
-      } else {
-        // Agregar un nuevo producto a la lista del carrito
-        const fila = document.createElement("tr");
-        fila.innerHTML = `
-          <td>${nombreProducto}</td>
-          <td>${unidades}</td>
-          <td>$${subtotal.toFixed(2)}</td>
-        `;
-        tablaBody.appendChild(fila);
+      if (producto) {
+        const unidades = parseInt(card.querySelector(".form-select").value);
+        agregarAlCarrito(producto, unidades);
       }
-
-      guardarPedidoEnLocalStorage(nombreProducto, unidades, subtotal);
     });
   });
 
-  btnVaciarCarrito.addEventListener("click", function() {
-    tablaBody.innerHTML = "";
-    vaciarLocalStorage();
+  tablaBody.addEventListener("click", function (event) {
+    if (event.target.classList.contains("btn-remover")) {
+      const nombreProducto = event.target.getAttribute("data-nombre");
+      removerDelCarrito(nombreProducto);
+    }
   });
 
-  function guardarPedidoEnLocalStorage(nombre, cantidad, subtotal) {
-    let pedidos = obtenerPedidosDeLocalStorage();
+  btnVaciarCarrito.addEventListener("click", function () {
+    carrito = [];
+    guardarPedidoEnLocalStorage();
+    mostrarCarrito();
+  });
 
-    // Verificar si el pedido ya existe en el Local Storage
-    const pedidoExistente = pedidos.find(pedido => pedido.nombre === nombre);
-
-    if (pedidoExistente) {
-      // Actualizar la cantidad y subtotal del pedido existente
-      pedidoExistente.cantidad += cantidad;
-      pedidoExistente.subtotal += subtotal;
-    } else {
-      // Agregar un nuevo pedido al arreglo
-      pedidos.push({ nombre, cantidad, subtotal });
-    }
-
-    localStorage.setItem("pedidos", JSON.stringify(pedidos));
+  function guardarPedidoEnLocalStorage() {
+    localStorage.setItem("pedidos", JSON.stringify(carrito));
   }
 
   function obtenerPedidosDeLocalStorage() {
     let pedidos = localStorage.getItem("pedidos");
-    
-    if (pedidos === null) {
-      return [];
-    } else {
-      return JSON.parse(pedidos);
-    }
+    return pedidos ? JSON.parse(pedidos) : [];
   }
 
-  function vaciarLocalStorage() {
-    localStorage.removeItem("pedidos");
-  }
+  // Cargar datos desde JSON local
+  let productos = [];
+  fetch("productos.json")
+    .then((response) => response.json())
+    .then((data) => {
+      productos = data; // Array de objetos con los datos de los productos
+      mostrarCarrito();
+    })
+    .catch((error) => console.error("Error al cargar los productos:", error));
 });
